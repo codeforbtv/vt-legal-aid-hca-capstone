29763960a7b470b1588490b74d8219bbb2179b5b
Eugene <eugene.yum@gmail.com>
2022-03-13 22:03:35 -0400

    removing some lint warnings

--- src/components/NavAdmin.js
+++ src/components/NavAdmin.js
@@ -1,19 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import LegalAidLogo1 from '../LegalAidLogo1.jpg'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-
-function refreshPage () {
-  window.location.reload(false)
-}
 
 export default function NavAdmin (props) {
   return (
