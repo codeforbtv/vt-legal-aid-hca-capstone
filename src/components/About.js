55b2db79c602048e1dd8ac349dc40887d861cdcf
Terri Isidro <terri.isidro@gmail.com>
2022-03-14 11:22:46 -0400

    appreciation text and links added to About page

--- src/components/About.js
+++ src/components/About.js
@@ -110,8 +110,8 @@ export default function About () {
             <ol>
               <li>
                 Inform policy makers and the public about the issue of medical
-                debt and its negative impact on goal of Vermonters getting the
-                right care at the right time.
+                debt and its negative impact on getting the right care at the
+                right time.
               </li>
               <li>
                 Support the passage of meaningful policy changes to reduce
@@ -131,8 +131,52 @@ export default function About () {
               Please share your story here.
             </a>
           </div>
+          <div class='content'>
+            Terri Isidro, Paul Dodson, Matthew Crownover, and Michael Pendleton
+            developed this website as a capstone project for the{' '}
+            <a href='https://burlingtoncodeacademy.com/' target='_blank'>
+              Burlington Code Academy (BCA)
+            </a>
+            .{' '}
+            <a href='https://burlingtoncodeacademy.com/' target='_blank'>
+              BCA
+            </a>{' '}
+            is an award-winning technology education center that helps adults of
+            all backgrounds change careers through online software development
+            and UX/UI design training programs.{' '}
+            <a href='https://burlingtoncodeacademy.com/' target='_blank'>
+              BCA's
+            </a>{' '}
+            array of part-time and full-time courses emphasize teamwork, career
+            readiness, and the value of collaborative learning in the
+            21st-century workplace.{' '}
+            <a href='https://burlingtoncodeacademy.com/' target='_blank'>
+              BCA
+            </a>{' '}
+            fuses education with practical career training to ensure students’
+            long-term success.
+          </div>
+          <div class='content'>
+            This site is also made possible by the past and ongoing support of
+            <a href='https://codeforbtv.org/'>Code for BTV</a>.{' '}
+            <a href='https://codeforbtv.org/'>Code for BTV</a> is an official
+            local chapter (known as a Brigade) of the national program
+            <a href='https://brigade.codeforamerica.org/'>
+              Code for America
+            </a>. <a href='https://codeforbtv.org/'>Code for BTV</a>
+            facilitates sustainable collaborations on civic software and open
+            data projects between coders, designers, and organizations (both
+            governmental and non-governmental) in Vermont. Effectively,{' '}
+            <a href='https://codeforbtv.org/'>Code for BTV</a> acts as a pro
+            bono digital agency for our partners, covering all aspects of
+            project design, development, and delivery.
+          </div>
+          <div class='content'>
+            The HCA is deeply grateful for the support and contributions that
+            these persons and organizations generously gave to us. This web site
+            would not be possible but for their generosity.
+          </div>
         </div>
-
         <div class='right-article'>
           <img src={Debt2} alt='legal debt' />
         </div>
